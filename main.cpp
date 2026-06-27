6b30a9f4e0500fe8579efcff9f4147c8891d3df9
sabr_79 <sabirinm07976@gmail.com>
2026-06-27 14:46:06 -0500

    first image, added matplot for mac friendly image displaying

--- main.cpp
+++ main.cpp
@@ -1,6 +1,28 @@
 #include <iostream>
+using namespace std;
 
 int main() {
-    std::cout << "Hello, Ray Tracer!" << std::endl;
-    return 0;
+   //cout << "Hello, Ray Tracer!" << endl;
+    
+
+    int width = 256;
+    int height = 256;
+
+    cout << "P3\n" << width << ' ' << height << "\n255\n";
+
+    for (int j = 0; j < height; j++){
+        for (int i = 0; i < width; i++){
+            auto r = double(i) / (width -1);
+            auto g = double(j) / (height-1);
+            auto b = 0.0;
+
+            int ir = int(255.999 * r);
+            int ig = int(255.999 * g);
+            int ib = int(255.999 * b);
+
+            cout << ir << ' ' << ig << ' ' << ib << '\n';
+        }
+    }
+
+    
 }
---
+++ viewer.py
@@ -0,0 +1,11 @@
+# python script to display ppm files since it isn't compatible with Mac's preview
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+
+# Read the PPM file y)
+img = mpimg.imread("image.ppm")
+
+
+plt.imshow(img)
+plt.axis('off')  
+plt.show()
