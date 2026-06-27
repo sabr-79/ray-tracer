767e84d7c3a3217244bc601f83522619e2966168
sabr_79 <sabirinm07976@gmail.com>
2026-06-27 16:05:55 -0500

    added vec3 and color header files, created image portfolio

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # Ray Tracer
+
+To-do:
+-Get a simple, general ray tracer working: baseline
+-Add BVH
+-Apple Optimizations: GCD, vDSP, Metal for M2
+-Benchmark General vs Optimized
+
--- main.cpp
+++ main.cpp
@@ -1,28 +1,23 @@
 #include <iostream>
+#include "vec3.h"
+#include "color.h"
 using namespace std;
 
 int main() {
-   //cout << "Hello, Ray Tracer!" << endl;
+   clog << "Hello, Ray Tracer!\n";
     
 
     int width = 256;
     int height = 256;
 
-    cout << "P3\n" << width << ' ' << height << "\n255\n";
+    cout << "P3\n" << width << " " << height << "\n255\n";
 
-    for (int j = 0; j < height; j++){
+    for (int j = 0; j < height; j++){ 
+        //clog << "Scanlines remaining: " << (height - j) << "\n";
         for (int i = 0; i < width; i++){
-            auto r = double(i) / (width -1);
-            auto g = double(j) / (height-1);
-            auto b = 0.0;
-
-            int ir = int(255.999 * r);
-            int ig = int(255.999 * g);
-            int ib = int(255.999 * b);
-
-            cout << ir << ' ' << ig << ' ' << ib << '\n';
+            auto pixel_color = color(double(i)/(width-1), 0, double(j)/(height-1));
+            write_color(cout, pixel_color);
         }
     }
-
-    
+    clog << "\rDone.                 \n"; 
 }
