847c9581914408f236d97d555b0307d07b44900a
sabr_79 <sabirinm07976@gmail.com>
2026-07-06 12:35:55 -0500

    updates to camera: can change position and field of view for scenes

--- main.cpp
+++ main.cpp
@@ -15,10 +15,12 @@ int main() {
 
     hittable_list world;
 
+    //auto R = std::cos(pi/4);
+
     auto material_ground = make_shared<lambertian>(color(0.8, 0.8, 0.0));
     auto material_center = make_shared<lambertian>(color(0.1, 0.2, 0.5));
     auto material_left   = make_shared<dielectric>(1.50);
-    auto material_bubble = make_shared<dielectric>(1.00/1.50);
+    auto material_bubble = make_shared<dielectric>(1.00 / 1.50);
     auto material_right  = make_shared<metal>(color(0.8, 0.6, 0.2), 1.0);
 
     world.add(make_shared<sphere>(point3( 0.0, -100.5, -1.0), 100.0, material_ground));
@@ -37,6 +39,10 @@ int main() {
     cam.width  = 400;
     cam.samples_per_pixel = 100;
     cam.max_depth = 50; 
+    cam.vfov = 20;
+    cam.lookfrom = point3(-2, 2, 1);
+    cam.lookat = point3(0, 0, -1);
+    cam.vup = vec3(0,1,0);
 
     cam.render(world);
     
